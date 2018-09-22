379147ac57e6c274539a2f98532828d69e3e62c2
yasinshaw <yasinshaw@gmail.com>
2018-09-22 10:26:49 +0800

    update some style

--- js/common.js
+++ js/common.js
@@ -1,7 +1,21 @@
 hljs.initHighlightingOnLoad();
-setTimeout(() => {
-    $('code.hljs').each(function(){
-        var codestr = $(this).html();
-        $(this).html(codestr.replace(/\n/g, '<br>'));
-    });
-}, 3000);
+
+var codeSpace = new RegExp( '    ' , "g" )
+
+$(document).ready(function(){
+    setTimeout(() => {
+        $('code.hljs').each(function(){
+            var codestr = $(this).html();
+            $(this).html(codestr.replace(/\n/g, '<br>'));
+        });
+    }, 3000);
+});
+
+$(document).ready(function(){
+    setTimeout(() => {
+        $('code.hljs').each(function(){
+            var codestr = $(this).html();
+            $(this).html(codestr.replace(codeSpace, '  '));
+        });
+    }, 10000);
+});
